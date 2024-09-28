21340c830b0e8d79e80f79da8967d2de01f8e069
Yichen Liu <yl127@illinois.edu>
2024-09-28 16:31:54 -0700

    save work for interactive plot. stretch works but unable to hide axis label when using wcs

--- autils/jupyter.py
+++ autils/jupyter.py
@@ -3,54 +3,75 @@ from ipywidgets import Layout
 from IPython.display import display, HTML
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from astropy.visualization import ImageNormalize, LinearStretch, AsinhStretch, ZScaleInterval
+
+np.seterr(invalid='warn')
+
+
+class __CustomNorm(Normalize):
+
+    def __init__(self, stretch_func=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stretch_func = stretch_func
+
+    def __call__(self, value, clip=None):
+        if self.stretch_func is not None:
+            value = self.stretch_func(value)
+        return super().__call__(value, clip)
 
 def __make_plot(data, 
             scale_method, scale_range, 
-            scale_slider, colormap, grid, coordinate, 
+            scale_slider, colormap, show_grid, coordinate, 
             wcs=None):
-    plt.figure(figsize=[8, 6])
-    # parse scaling method
-    if scale_method=='linear': 
-        strech = lambda x: x
-    elif scale_method=='log':
-        strech = np.log
-    elif scale_method=='power':
-        strech = np.exp
-    elif scale_method=='sqrt':
-        strech = np.sqrt
-    elif scale_method=='squared':
-        strech = lambda x: x**2
-    elif scale_method=='asinh': 
-        strech = np.arcsinh
-    elif scale_method=='sinh': 
-        strech = np.sinh
-    elif scale_method=='histogram': 
-        strech = NotImplemented
+    data = np.asarray(data, dtype=np.float64)
+    fig = plt.figure(figsize=[8, 6])
+    # parse coordinate
+    if coordinate=='world':
+        ax = fig.add_subplot(111, projection=wcs)
+        ax.coords[0].set_axislabel(None)  # RA label
+        ax.coords[1].set_ticklabel_visible(False)  # Hide the Dec label
+        
+    if coordinate=='image':
+        ax = fig.add_subplot(111)
     else: 
-        strech = NotImplemented
-    # parse scale_range and scale_slider
-    vmin = NotImplemented
-    vmax = NotImplemented
-    # TODO: parse coordinate
-    # TODO: parse wcs
-    has_wcs = wcs is not None
-    # TODO: temp imshow
-    plt.imshow(strech(data)*scale_slider*2, vmin=np.min(strech(data)), vmax=np.max(strech(data)), cmap=colormap)
-    plt.colorbar(orientation='horizontal', fraction=0.046, pad=0.04)
-    # parse grid
-    if grid: 
-        plt.grid('off')
+        pass
+    # parse scale_range
+    if scale_range=='min max':
+        vmin = np.min(data)
+        vmax = np.max(data)
+    elif scale_range=='zscale': 
+        zscale_interval = ZScaleInterval()
+        vmin, vmax = zscale_interval.get_limits(data)
     else: 
-        plt.grid()
-    plt.tight_layout()
-    plt.show()
+        pass
+    # TODO: parse scale_slider
+    # parse scale_method
+    norm = ImageNormalize(stretch=scale_method(), vmin=vmin, vmax=vmax)
 
+
+    # if coordinate
+    # TODO: imshow. parse colormap here
+    im = ax.imshow(data, cmap=colormap, norm=norm)
+    # fig.colorbar(im, ax=ax, orientation='horizontal', fraction=0.046, pad=0.04)
+    # parse show_grid
+    ax.grid(show_grid)
+    # show image
+    fig.tight_layout()
+    plt.show()
 
 def interactive_plot(data, wcs=None):
 
     scale_method = widgets.ToggleButtons(
-        options=['linear', 'log', 'power', 'sqrt', 'squared', 'asinh', 'sinh', 'histogram'],
-        value='linear', 
+        options=[('linear', LinearStretch), 
+                #  ('log', AsinhStretch), 
+                #  ('power', AsinhStretch), 
+                #  ('sqrt', AsinhStretch), 
+                #  ('squared', AsinhStretch), 
+                #  ('asinh', AsinhStretch), 
+                #  ('sinh', AsinhStretch), 
+                 ('histogram', AsinhStretch)],
+        value=LinearStretch, 
         layout=Layout(width='80%', height='33px'), 
         description='',
         disabled=False, 
@@ -85,7 +106,7 @@ def interactive_plot(data, wcs=None):
         layout=Layout(width='10%'), 
     )
 
-    grid = widgets.ToggleButton(
+    show_grid = widgets.ToggleButton(
         value=False,
         description='grid',
         disabled=False,
@@ -99,7 +120,7 @@ def interactive_plot(data, wcs=None):
     coordinate = widgets.ToggleButtons(
         options=['image', 'world'],
         value='image',
-        layout=Layout(width='20.2%', height='33px'), 
+        layout=Layout(width='20%', height='33px'), 
         description='',
         disabled=no_wcs, 
         style={'button_width': '47%'}
@@ -117,7 +138,7 @@ def interactive_plot(data, wcs=None):
     """
 
     ui1 = widgets.HBox([scale_method, scale_range])
-    ui2 = widgets.HBox([scale_slider, colormap, grid, coordinate])
+    ui2 = widgets.HBox([scale_slider, colormap, show_grid, coordinate])
     ui = widgets.VBox([ui1, ui2])
 
     out = widgets.interactive_output(__make_plot, 
@@ -126,7 +147,7 @@ def interactive_plot(data, wcs=None):
                                     'scale_range': scale_range, 
                                     'scale_slider': scale_slider, 
                                     'colormap': colormap, 
-                                    'grid': grid,
+                                    'show_grid': show_grid,
                                     'coordinate': coordinate, 
                                     'wcs':  widgets.fixed(wcs)})
 
